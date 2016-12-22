546ee69d50d8e26e738a90500a982736bbad26bf
Michael Agostino <mike.agostino92@gmail.com>
2016-12-22 17:12:25 -0500

    Added functionality of biweekly/monthly payments

--- www/controllers/singleController.js
+++ www/controllers/singleController.js
@@ -1,35 +1,37 @@
 angular.module('app.controllers')
 app.controller("singleCtrl", ['$scope', function singleCtrl($scope) {
-    $scope.CarInfo = {
-        value: 0,
-        financeRate: 0,
-        years: 1,
-        tax: 0,
-        down: 0
+
+    $scope.CarInfo = {};
+    $scope.CarInfo.value = 0;
+    $scope.CarInfo.financeRate = 0;
+    $scope.CarInfo.years = 1;
+    $scope.CarInfo.tax = 0;
+    $scope.CarInfo.down = 0;
+
+    $scope.biweekly = 0;
+    $scope.monthly = 0;
+
+
+
+
+    $scope.getMonthlyInterest = function () {
+        var monthly_interest = ($scope.CarInfo.financeRate / 100) / 12;
+
+        var monthly_payments = 0.0;
+        var months = $scope.CarInfo.years * 12;
+        var monthly_interest = ($scope.CarInfo.financeRate / 100) / 12;
+        monthly_payments = ($scope.CarInfo.value) * (((monthly_interest) * Math.pow(1 + monthly_interest, months)) / (Math.pow(1 + monthly_interest, months) - 1));
+        $scope.monthly = Math.round(monthly_payments);
+        return $scope.monthly;
     };
 
-    $scope.getMonthlyInterest = getMonthlyInterest;
-    $scope.convertToBiweekly = convertToBiweekly;
-
-    $scope.monthly = getMonthlyInterest(value,financeRate,years);
-    $scope.biweekly = convertToBiweekly($scope.monthly);
-    
-
-    $scope.calculate = function calculate($scope) {
-         var monthly_interest = (interest / 100) / 12;
+    $scope.convertToBiweekly = function convertToBiweekly(payments) {
+        $scope.biweekly = (payments * 12) / 26;
     };
 
-    $scope.getMonthlyInterest = function getMonthlyInterest(price,interest,years){
-            var monthly_payments = 0.0;
-            var months = years * 12;
-            var monthly_interest = (interest / 100) / 12;
-            monthly_payments = (price) * (((monthly_interest) * Math.Pow(1 + monthly_interest, months)) / (Math.Pow(1 + monthly_interest, months) - 1));
-            return Math.Round(monthly_payments, 2);
-    };
-
-    $scope.convertToBiweekly = function convertToBiweekly(payments){
-            var biweekly = (payments * 12) / 26;
-            return Math.Round(biweekly, 2);
-    };
+    $scope.calculate = function calculate() {
+        var monthlyPayments = $scope.getMonthlyInterest();
+        $scope.convertToBiweekly(monthlyPayments);
+    };    
 }]
 );
